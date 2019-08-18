432f8cfa2d16322080e80549a57007c02f0f81c3
Stepa-Kononovich <stepakononovich@gmail.com>
2019-08-18 13:39:08 +0300

    push range

--- Main_script.js
+++ Main_script.js
@@ -12,3 +12,47 @@ function handleClick(e) {
 document.addEventListener('click', handleClick);
 document.addEventListener('touch', handleClick);
 
+const container = document.querySelector('.range_container');
+const range = document.querySelector('.button_range');
+let shiftX = null;
+let difference = null;
+let rangeX = null;
+let containerValueCost = document.querySelector('.end_value');
+
+
+console.dir(range)
+function movRange(event){
+    shiftX = event.pageX || event.touches[0].pageX;
+    let x = shiftX-container.getBoundingClientRect().left-difference;
+    if(x < 0) {
+        x = 0;
+    }
+
+    if(x > container.getBoundingClientRect().width) {
+        x = container.getBoundingClientRect().width;
+    }
+    range.style.left =`${x}px`;
+    let max = 3000;
+    let min = 0;
+    containerValueCost.innerHTML = Math.round((max/100)*((range.offsetLeft/container.getBoundingClientRect().width)*100));
+}
+
+
+function mouhtMoveAdd(event) {
+    rangeX = range.getBoundingClientRect().left;
+    shiftX = event.pageX || event.touches[0].pageX;
+    difference = shiftX - rangeX;
+    document.addEventListener('mousemove', movRange);
+    document.addEventListener('touchmove', movRange);
+}
+
+function mouhtMoveRemove() {
+    document.removeEventListener('mousemove', movRange);
+    document.removeEventListener('touchmove', movRange);
+}
+
+range.addEventListener('mousedown', mouhtMoveAdd);
+range.addEventListener('touchstart', mouhtMoveAdd);
+
+document.addEventListener('mouseup', mouhtMoveRemove);
+document.addEventListener('touchend', mouhtMoveRemove);
